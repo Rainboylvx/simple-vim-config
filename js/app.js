cb5d9781ec0caed712a5276b0c2216814d5839f8
Rainboy <597872644@qq.com>
2017-01-03 22:44:16 +0800

    a lot

---
+++ js/app.js
@@ -0,0 +1,23 @@
+//编码
+
+var annc = {}
+
+
+annc.render = function(content){
+  var str_vim = he.encode(content);
+
+  //$('#content').append("<div>"+str_vim+"</div");
+  $('#content').append(str_vim);
+  Prism.highlightAll();
+}
+
+
+annc.getAndRender  = function(url){
+  $.get(url,function(data,status){
+    annc.render(data);
+  })
+}
+
+annc.run = function(){
+  this.getAndRender('.vimrc')
+}
